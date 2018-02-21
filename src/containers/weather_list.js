229b449737e62417f420de427f549f47f782cf30
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-21 14:24:46 -0500

    Create connected version of weather_list

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+
+
         </tbody>
       </table>
       );
-
   }
 }
+
+// Pull weather data into container
+// Use state.weather because we defined weather in the rootReducer in index.js
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+
+
+// ES6 change: Instead of
+// function mapStateToProps(state) {
+//   return { weather: state.weather };
+// }
+// Use:
+// function mapStateToProps({ weather }) {
+//   return { weather };
+// }
+
+
+// When you add this, remove export default from in front of class WeatherList
+export default connect(mapStateToProps)(WeatherList);
+
