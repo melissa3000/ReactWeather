15c83cb5995789ec7f1f972022462898ac668190
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-21 15:06:58 -0500

    refactor sparklines into separate reusable component

---
+++ src/components/chart.js
@@ -0,0 +1,12 @@
+// State not required inside chart, so make this a functional component and not
+// a class based component.
+
+import React from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+export default (props) => {
+  return (
+    <div>
+    </div>
+    );
+}
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -26,9 +26,6 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
         </td>
       </tr>
       )
