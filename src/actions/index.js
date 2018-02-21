27943f7118fa02d5e003cdea010edc795502b4a7
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-21 12:57:24 -0500

    Added API request as ajax request using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,29 @@
-const API_KEY = '100ef4656c83a82e87aebf7662784d32';
+import axios from 'axios';
 
+const API_KEY = '100ef4656c83a82e87aebf7662784d32';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
 // Middleware has the ability to block, modify or let pass through actions as
 // they're created before they hit a reducer. Think of it as a gatekeeper.
+
+// Application state holds all of the data of our application. We only change our
+// application state through reducers and actions. So, to load weather data
+// we need to dispatch an action (call an action creator) which is responsible
+// for making the ajax request.
+
+// Action creator makes API request to fetch weather data. Use Axios instead of
+// jquery to make Ajax request
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}, us`;
+  const request = axios.get(url);
+
+  // All actions must return a type, some also return a payload
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
