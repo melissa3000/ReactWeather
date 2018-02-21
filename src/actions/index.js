a956be9089497fb9c06cadab0b508bccb788589d
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-20 21:56:47 -0500

    Added comments explaining middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,6 @@
 const API_KEY = '100ef4656c83a82e87aebf7662784d32';
 
+
+
+// Middleware has the ability to block, modify or let pass through actions as
+// they're created before they hit a reducer. Think of it as a gatekeeper.
