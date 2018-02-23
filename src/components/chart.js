29200e4bf72b64424f933348aaff50378d58cc7b
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-22 19:46:03 -0500

    Created chart functional component to display sparklines

--- src/components/chart.js
+++ src/components/chart.js
@@ -7,6 +7,9 @@ import { Sparklines, SparklinesLine } from 'react-sparklines';
 export default (props) => {
   return (
     <div>
+      <Sparklines height={120} width={180} data={temps}>
+        <SparklinesLine color="red" />
+      </Sparklines>
     </div>
     );
 }
