7be5110d6efc99aea591f44af28258b2894b05cf
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-24 11:31:23 -0500

    Added css so that graphs render in uniform size

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,6 +6,8 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    // If you decide to change units:
+    // const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp -273);
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
