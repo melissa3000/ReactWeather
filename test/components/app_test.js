946be8e8f8ae19941c7700ecee730f7be7ff41bf
Melissa Boyer <melissaboyer3000@gmail.com>
2021-01-27 09:08:04 -0500

    another short test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 //This is a placeholder comment to test something
+// more testing
 
 describe('App' , () => {
   let component;
